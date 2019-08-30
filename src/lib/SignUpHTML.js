6ee2cbe82132057b9f3eb99398cecac09c14d607
Einere <kjwsx23@gmail.com>
2019-08-30 11:27:54 +0900

    SignUpHTML객체의 필드로 validation을 추가. 향후 회원가입 버튼 클릭시 체크하기 위함.
    Signed-off-by: Einere <kjwsx23@gmail.com>

--- src/lib/SignUpHTML.js
+++ src/lib/SignUpHTML.js
@@ -1,5 +1,18 @@
 function SignUpHTML() {
     this.days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    this.validation = {
+        id: false,
+        pw: false,
+        pwCheck: false,
+        name: false,
+        year: false,
+        month: false,
+        day: false,
+        gender: false,
+        email: false,
+        phone: false,
+        interesting: false
+    };
 }
 
 SignUpHTML.prototype.getHtml = function () {
@@ -86,11 +99,13 @@ SignUpHTML.prototype.setEventListenerToId = function () {
         if (regexp.test(e.target.value)) {
             formIdResult.textContent = '사용 가능한 아이디입니다.';
             formIdResult.style.color = "green";
+            this.validation.id = true;
         } else {
             formIdResult.textContent = '5~20자의 영문 소문자, 숫자와 특수기호(_)(-) 만 사용 가능합니다.';
             formIdResult.style.color = "red";
+            this.validation.id = false;
         }
-    });
+    }.bind(this));
 };
 
 
@@ -101,26 +116,31 @@ SignUpHTML.prototype.setEventListenerToPw = function () {
         if (!/^.{8,16}$/.test(e.target.value)) {
             formPwResult.textContent = '8자 이상 16자 이하로 입력해주세요.';
             formPwResult.style.color = "red";
+            this.validation.pw = false;
             return;
         }
         if (!/[A-Z]+/.test(e.target.value)) {
             formPwResult.textContent = '영문 대문자를 최소 1자 이상 포함해주세요.';
             formPwResult.style.color = "red";
+            this.validation.pw = false;
             return;
         }
         if (!/[0-9]+/.test(e.target.value)) {
             formPwResult.textContent = '숫자를 최소 1자 이상 포함해주세요.';
             formPwResult.style.color = "red";
+            this.validation.pw = false;
             return;
         }
         if (!/[!@#$%^&*]+/.test(e.target.value)) {
             formPwResult.textContent = '특수문자를 최소 1자 이상 포함해주세요.';
             formPwResult.style.color = "red";
+            this.validation.pw = false;
             return;
         }
         formPwResult.textContent = '안전한 비밀번호입니다.';
         formPwResult.style.color = "green";
-    });
+        this.validation.pw = true;
+    }.bind(this));
 };
 
 SignUpHTML.prototype.setEventListenerToPwCheck = function () {
@@ -131,11 +151,13 @@ SignUpHTML.prototype.setEventListenerToPwCheck = function () {
         if (formPw.value !== e.target.value) {
             formPwCheckResult.textContent = '비밀번호가 일치하지 않습니다.';
             formPwCheckResult.style.color = "red";
+            this.validation.pwCheck = false;
             return;
         }
         formPwCheckResult.textContent = '비밀번호가 일치합니다.';
         formPwCheckResult.style.color = "green";
-    });
+        this.validation.pwCheck = true;
+    }.bind(this));
 };
 
 SignUpHTML.prototype.setEventListenerToYear = function () {
@@ -146,19 +168,25 @@ SignUpHTML.prototype.setEventListenerToYear = function () {
         if (isNaN(birthYear)) {
             formBirthResult.textContent = '태어난 년도 4자리를 정확하게 입력하세요.';
             formBirthResult.style.color = 'red';
+            this.validation.year = false;
             return;
         }
         const age = new Date().getFullYear() - birthYear;
         if (age < 15 || age > 99) {
             formBirthResult.textContent = '15세 이상, 99세 이하만 가입할 수 있습니다.';
             formBirthResult.style.color = 'red';
+            this.validation.year = false;
             return;
         }
         formBirthResult.textContent = '';
         formBirthResult.style.color = 'green';
-
-    });
+        this.validation.year = true;
+    }.bind(this));
 };
+
+function isLeapYear(year) {
+    return (year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0);
+}
 
 SignUpHTML.prototype.setEventListenerToDay = function (e) {
     document.getElementById('form-day').addEventListener('blur', function (e) {
@@ -170,16 +198,26 @@ SignUpHTML.prototype.setEventListenerToDay = function (e) {
         if (isNaN(birthMonth)) {
             formBirthResult.textContent = '태어난 월을 선택해주세요.';
             formBirthResult.style.color = 'red';
+            this.validation.month = false;
             return;
         }
+
+
+        const formBirthYear = document.querySelector('#form-year');
+        this.days[1] = isLeapYear(formBirthYear.value) ? 29 : 28;
+        console.log(this.days[1]);
 
         if (birthDay < 1 || birthDay > this.days[birthMonth]) {
             formBirthResult.textContent = '태어난 날짜를 다시 확인해주세요.';
             formBirthResult.style.color = 'red';
+            this.validation.day = false;
             return;
         }
+
         formBirthResult.textContent = '';
         formBirthResult.style.color = 'green';
+        this.validation.month = true;
+        this.validation.day = true;
     }.bind(this));
 };
 
@@ -190,11 +228,13 @@ SignUpHTML.prototype.setEventListenerToGender = function () {
         if (isNaN(parseInt(e.target.value, 10))) {
             formGenderResult.textContent = '성별을 선택해주세요.';
             formGenderResult.style.color = 'red';
+            this.validation.gender = false;
             return;
         }
         formGenderResult.textContent = '';
         formGenderResult.style.color = 'green';
-    });
+        this.validation.gender = true;
+    }.bind(this));
 };
 
 SignUpHTML.prototype.setEventListenerToEmail = function () {
@@ -206,11 +246,13 @@ SignUpHTML.prototype.setEventListenerToEmail = function () {
         if (!regexp.test(formEmail.value)) {
             formEmailResult.textContent = '이메일 주소를 다시 확인해주세요.';
             formEmailResult.style.color = 'red';
+            this.validation.email = false;
         } else {
             formEmailResult.textContent = '';
             formEmailResult.style.color = 'green';
+            this.validation.email = true;
         }
-    });
+    }.bind(this));
 };
 
 SignUpHTML.prototype.setEventListenerToPhone = function () {
@@ -222,11 +264,13 @@ SignUpHTML.prototype.setEventListenerToPhone = function () {
         if (!regexp.test(formPhone.value)) {
             formPhoneResult.textContent = '형식에 맞지 않는 번호입니다.';
             formPhoneResult.style.color = 'red';
+            this.validation.phone = false;
             return;
         }
         formPhoneResult.textContent = '';
         formPhoneResult.style.color = 'green';
-    });
+        this.validation.phone = true;
+    }.bind(this));
 };
 
 export {SignUpHTML};
